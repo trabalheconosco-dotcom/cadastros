09341b01154924d7404b078968dc1be260aa8cde
trabalheconosco-dotcom <trabalheconosco@comtrasil.com.br>
2026-07-24 08:29:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL_SCRIPT = "https://script.google.com/a/macros/comtrasil.com.br/s/AKfycbyBWJxerrdVQMU9vLAIPNdzC_aC83HnXXpRouauCLD9nFHsxZBBig_znVbKy0Xr-D5g/exec";
+const URL_SCRIPT = "https://script.google.com/a/macros/comtrasil.com.br/s/AKfycbwC6uniGeZ-cF99Hja1bRWC6suCJY7Y23vyBt9GOKeyRvNrRrRl5Lo2iGU-5xFbvOB_/exec";
 
 
 function gerarNumero(){
