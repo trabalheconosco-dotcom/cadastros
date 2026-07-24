1ccac804082d0d73aade88cf2d9ca214b71d0e10
trabalheconosco-dotcom <trabalheconosco@comtrasil.com.br>
2026-07-24 11:48:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function cadastrar() {
             <h3>Cadastro realizado!</h3>
             <p>Seu número é:</p>
             <h1>${numero}</h1>
+            <h3></h3>
         `;
 
         nomeInput.value = "";
@@ -39,4 +40,21 @@ function cadastrar() {
         console.error("Erro no envio:", error);
         alert("Erro ao enviar cadastro");
     });
+}
+
+// Função para gerar o print de qualquer elemento
+function tirarPrint(idDoElemento, nomeDoArquivo = 'comprovante.png') {
+  const elemento = document.getElementById(idDoElemento);
+
+  if (!elemento) {
+    console.error("Elemento não encontrado!");
+    return;
+  }
+
+  html2canvas(elemento).then(canvas => {
+    const link = document.createElement('a');
+    link.download = nomeDoArquivo;
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+  });
 }
